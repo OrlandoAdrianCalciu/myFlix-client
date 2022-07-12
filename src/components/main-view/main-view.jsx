8144652c9cf852eaa4e1edd98bb5544469e171e5
Orlando-Adrian Calciu <orlandoadrian5444@outlook.com>
2022-07-12 12:21:29 +0100

    Update main-view.jsx

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -8,9 +8,9 @@ export class MainView extends React.Component {
         super();
         this.state = {
             movies: [
-                { _id: 1, Title: 'Inception', Description: 'desc1...', ImagePath: '...' },
-                { _id: 2, Title: 'The Shawshank Redemption', Description: 'desc2...', ImagePath: '...' },
-                { _id: 3, Title: 'Gladiator', Description: 'desc3...', ImagePath: '...' }
+                { _id: 1, Title: 'Life', Description: 'A team of scientists aboard the International Space Station discover a rapidly evolving life from that caused extinction on Mars and now threatens all life on Earth.', ImagePath: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSL8hcAOmSG7nJVmkhrnqlaDThH5r0Zj1R4j6kzDx2BXhvVXETI' },
+                { _id: 2, Title: 'The Mummy', Description: 'An ancient Egyptian princess is awakened from her crypt beneath the desert, bringing with her malevolence grown over millennia, and terrors that defy human comprehension.', ImagePath: 'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTCXgCV-ZNb3InBCTaLdED58dF6iZJxIvCOBurktiWxXrwGc8DB' },
+                { _id: 3, Title: 'Gifted', Description: 'Frank, a single man raising his child prodigy niece Mary, is drawn into a custody battle with his mother.', ImagePath: 'https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcSE8rmFQdCRPL8OpCknQJ2ScEcXqf39eOi9w-KIhPpb7aSUrdS3' }
             ],
             selectedMovie: null
         };
