150f8e2de434e3d138b9d3898ef2ca1c114a2e20
Orlando-Adrian Calciu <orlandoadrian5444@outlook.com>
2022-07-14 13:11:53 +0100

    Added Axios

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import axios from 'axios';
+
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
 
@@ -7,13 +9,21 @@ export class MainView extends React.Component {
     constructor() {
         super();
         this.state = {
-            movies: [
-                { _id: 1, Title: 'Life', Description: 'A team of scientists aboard the International Space Station discover a rapidly evolving life from that caused extinction on Mars and now threatens all life on Earth.', ImagePath: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSL8hcAOmSG7nJVmkhrnqlaDThH5r0Zj1R4j6kzDx2BXhvVXETI' },
-                { _id: 2, Title: 'The Mummy', Description: 'An ancient Egyptian princess is awakened from her crypt beneath the desert, bringing with her malevolence grown over millennia, and terrors that defy human comprehension.', ImagePath: 'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTCXgCV-ZNb3InBCTaLdED58dF6iZJxIvCOBurktiWxXrwGc8DB' },
-                { _id: 3, Title: 'Gifted', Description: 'Frank, a single man raising his child prodigy niece Mary, is drawn into a custody battle with his mother.', ImagePath: 'https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcSE8rmFQdCRPL8OpCknQJ2ScEcXqf39eOi9w-KIhPpb7aSUrdS3' }
-            ],
+            movies: [ ],
             selectedMovie: null
         };
+    }
+
+    componentDidMount(){
+      axios.get('https://top-movies-api.herokuapp.com/movies')
+      .then(response =>{
+        this.setState({
+          movies: response.data
+        });
+      })
+      .catch(error => {
+        console.log(error);
+      });
     }
 
     setSelectedMovie(newSelectedMovie){
@@ -25,14 +35,14 @@ export class MainView extends React.Component {
     render() {
         const { movies, selectedMovie } = this.state;
       
-        if (movies.length === 0) return <div className="main-view">The list is empty!</div>;
+        if (movies.length === 0) return <div className="main-view" />;
       
         return (
           <div className="main-view">
             {selectedMovie
               ? <MovieView movie={selectedMovie} onBackClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }}/>
               : movies.map(movie => (
-                <MovieCard key={movie._id} movie={movie} onMovieClick={(movie) => { this.setSelectedMovie(movie) }}/>
+                <MovieCard key={movie._id} movie={movie} onMovieClick={(newSelectedMovie) => { this.setSelectedMovie(newSelectedMovie) }}/>
               ))
             }
           </div>
