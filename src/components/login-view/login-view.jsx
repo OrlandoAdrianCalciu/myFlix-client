9f76f640e38d317e00f1e6b31643219a189cb584
Orlando-Adrian Calciu <orlandoadrian5444@outlook.com>
2022-07-20 15:26:06 +0100

    Update login-view.jsx

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import PropTypes from 'prop-types';
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
+import {Form, Button} from 'react-bootstrap';
+
 
 export function LoginView(props){
     const [ username, setUsername ] = useState('');
