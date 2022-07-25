60fed89d5267b8c5ee89358492107988ded22817
Orlando-Adrian Calciu <orlandoadrian5444@outlook.com>
2022-07-25 14:20:52 +0100

    Update

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -5,7 +5,7 @@ import { RegistrationView } from '../registration-view/registration-view';
 import { LoginView } from '../login-view/login-view';
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
-import  {Row, Col}  from 'react-bootstrap';
+import { Row, Col } from 'react-bootstrap';
 
 export class MainView extends React.Component {
 
@@ -20,13 +20,26 @@ export class MainView extends React.Component {
   }
 
   componentDidMount() {
-    axios.get('https://top-movies-api.herokuapp.com/movies')
+    let accessToken = localStorage.getItem('token');
+    if (accessToken !== null) {
+      this.setState({
+        user: localStorage.getItem('user')
+      });
+      this.getMovies(accessToken);
+    }
+  }
+
+  getMovies(token) {
+    axios.get('https://top-movies-api.herokuapp.com/movies', {
+      headers: { Authorization: `Bearer ${token}` }
+    })
       .then(response => {
+        //Assign the result to the state
         this.setState({
           movies: response.data
         });
       })
-      .catch(error => {
+      .catch(function (error) {
         console.log(error);
       });
   }
@@ -37,10 +50,15 @@ export class MainView extends React.Component {
     });
   }
 
-  onLoggedIn(user) {
+  onLoggedIn(authData) {
+    console.log(authData);
     this.setState({
-      user,
+      user: authData.user.Username
     });
+
+    localStorage.setItem('token', authData.token);
+    localStorage.setItem('user', authData.user.Username);
+    this.getMovies(authData.token);
   }
 
   onRegistration(registered) {
@@ -49,6 +67,13 @@ export class MainView extends React.Component {
     });
   }
 
+  onLoggedOut() {
+    localStorage.removeItem('token');
+    localStorage.removeItem('user');
+    this.setState({
+      user: null
+    });
+  }
 
 
   render() {
@@ -78,6 +103,7 @@ export class MainView extends React.Component {
 
     return (
       <div className="main-view">
+        <Button onClick={() => { this.onLoggedOut() }}>Logout</Button>
         {selectedMovie
           ? (
             <Row className="justify-content-md-center">
@@ -90,7 +116,7 @@ export class MainView extends React.Component {
             <Row className="justify-content-md-center">
               {movies.map(movie => (
                 <Col md={3}>
-                  <MovieCard key={movie._id} movie={movie} onMovieClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }}/>
+                  <MovieCard key={movie._id} movie={movie} onMovieClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie); }} />
                 </Col>
               ))}
             </Row>
--- src/components/registration-view/registration-view.jsx
+++ src/components/registration-view/registration-view.jsx
@@ -1,4 +1,3 @@
-
 import React, { useState } from "react";
 import PropTypes from 'prop-types';
 import { Form, Button, Card, CardGroup, Container, Col, Row } from "react-bootstrap";
