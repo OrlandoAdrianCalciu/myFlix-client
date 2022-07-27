d1aeb1085f469a1d2a6d6f6e13d6c16577d0b177
Orlando-Adrian Calciu <orlandoadrian5444@outlook.com>
2022-07-27 19:18:04 +0100

    Create director-view.jsx

---
+++ src/components/director-view/director-view.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import PropTypes from 'prop-types';
+import { Card, Col, Container, Row, Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
+
+export class DirectorView extends React.Component {
+
+
+    render() {
+        const { director, onBackClick } = this.props;
+
+        return (
+            <Card text='dark' className="director">
+                <Card.Header className="title">{director.Name}</Card.Header>
+                <Card.Body>
+                    <Card.Text>Born: {director.Birth}</Card.Text>
+                    <Card.Text> Biography: {director.Bio}</Card.Text>
+                    <Button variant="outline-danger" onClick={() => { onBackClick() }}>Back</Button>
+                </Card.Body>
+            </Card>
+        )
+    }
+}
