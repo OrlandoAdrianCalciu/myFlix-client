95051381b6eba868ddbc5a129c65b8dda8c02068
Orlando-Adrian Calciu <orlandoadrian5444@outlook.com>
2022-08-01 17:28:22 +0100

    Update Movie-Card

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,22 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
-
+import { Card } from 'react-bootstrap'
 import { Link } from 'react-router-dom';
+
+import "./movie-card.scss"
 
 export class MovieCard extends React.Component {
   render() {
     const { movie } = this.props;
 
     return (
-      <Card>
-        <Card.Img variant="top" src={movie.ImagePath} />
+      <Card className='card' style={{width: '15rem', height: '33rem'}}>
+        <Card.Img className='card-img' variant="top" src={movie.ImagePath} />
         <Card.Body>
-          <Card.Title>{movie.Title}</Card.Title>
-          <Card.Text>{movie.Description}</Card.Text>
+          <Card.Title className='card-title'>{movie.Title}</Card.Title>
+          {/* <Card.Text className='card-text'>{movie.Description}</Card.Text> */}
           <Link to={`/movies/${movie._id}`}>
-            <Button variant="link">Open</Button>
+            <Button className='open-button' variant="warning" size="sm">Open</Button>
           </Link>
         </Card.Body>
       </Card>
