bbc7bf72387093e7a158d5a79e6d8e2896c946b4
Orlando-Adrian Calciu <orlandoadrian5444@outlook.com>
2022-08-04 17:33:11 +0100

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -2,18 +2,25 @@ import React from 'react';
 import Container  from 'react-bootstrap/Container';
 import ReactDOM from 'react-dom';
 import MainView  from './components/main-view/main-view';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import moviesApp from './reducers/reducers';
 
 //Import statement to indicate that you need to bundle `./index.scss`
 import './index.scss';
+
+const store = createStore(moviesApp);
 
 //Main component (will eventually use all the others)
 class MyFlixApplication extends React.Component {
     render() {
         return (
+            <Provider store={store}>
             <Container className='body'>
                 <MainView />
             </Container>
-        )
+            </Provider>
+        );
     }
 }
 
